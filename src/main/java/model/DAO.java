ae7d8ac0309a40d1bb544cbda5a59696f5b4a1dc
Bruno Antunes <78450354+BrunoAntunes-Dev@users.noreply.github.com>
2022-02-21 23:51:44 -0300

    Update DAO.java

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -7,14 +7,14 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class DAO {
-	/** Modelo de conexão **/
-	// parametros de conexão
+	/**Modelo de conexão**/
+	//parametros de conexão
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
 	private String user = "root";
 	private String password = "totvs!133121";
-
-	// método de conexão
+	
+	//método de conexão
 	private Connection conectar() {
 		Connection con = null;
 		try {
@@ -26,31 +26,31 @@ public class DAO {
 			return null;
 		}
 	}
-
-	/** CRUD CREATE **/
+	
+	/**CRUD CREATE**/
 	public void inserirContato(JavaBeans contato) {
 		String create = "insert into contatos (nome, fone, email) values (?,?,?)";
-		try {
-			// abrir a conexão
-			Connection con = conectar();
-			// Praparar a query para execução no banco de dados
-			PreparedStatement pst = con.prepareStatement(create);
-			// Substituir os parâmetros (?) pelo conteúdo das variáveis JavaBeans
-			pst.setString(1, contato.getNome());
-			pst.setString(2, contato.getFone());
-			pst.setString(3, contato.getEmail());
-			// Executar a query
-			pst.executeUpdate();
-			// Encerrar a conexão com o banco
-			con.close();
+				try {
+					// abrir a conexão
+					Connection con = conectar();
+					// Praparar a query para execução no banco de dados
+					PreparedStatement pst = con.prepareStatement(create);
+					// Substituir os parâmetros (?) pelo conteúdo das variáveis JavaBeans
+					pst.setString(1, contato.getNome());
+					pst.setString(2, contato.getFone());
+					pst.setString(3, contato.getEmail());
+					// Executar a query
+					pst.executeUpdate();
+					// Encerrar a conexão com o banco
+					con.close();
 
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+				} catch (Exception e) {
+					System.out.println(e);
+				}
 	}
-
-	/** CRUD READ **/
-	public ArrayList<JavaBeans> listarContatos() {
+	
+	/**CRUD READ**/
+	public ArrayList<JavaBeans> listarContatos(){
 		// Criando um objeto para acessar a classe JavaBeans
 		ArrayList<JavaBeans> contatos = new ArrayList<>();
 		String read = "select * from contatos order by nome";
@@ -59,14 +59,14 @@ public class DAO {
 			PreparedStatement pst = con.prepareStatement(read);
 			ResultSet rs = pst.executeQuery();
 			// o laço abaixo será executado enquanto houver contatos
-			while (rs.next()) {
+			while(rs.next()) {
 				// variaveis de apoio que recebem os dados do banco
 				String idcon = rs.getString(1);
 				String nome = rs.getString(2);
 				String fone = rs.getString(3);
 				String email = rs.getString(4);
 				// populando o ArrayList
-				contatos.add(new JavaBeans(idcon, nome, fone, email));
+				contatos.add(new JavaBeans(idcon,nome,fone,email));
 			}
 			con.close();
 			return contatos;
@@ -75,10 +75,16 @@ public class DAO {
 			return null;
 		}
 	}
-
-	/**
-	 * teste de conexão public void testeConexao() { try { Connection con =
-	 * conectar(); System.out.println(con); con.close(); } catch (Exception e) {
-	 * System.out.println(e); } }
-	 **/
+	
+	
+	/**teste de conexão
+	public void testeConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}**/
 }
