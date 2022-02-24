224f61908cbddca014d57a1a0f056a880935efe1
Bruno Antunes <78450354+BrunoAntunes-Dev@users.noreply.github.com>
2022-02-23 23:57:58 -0300

    CRUD DELETE - Confirmação

---
+++ src/main/webapp/scripts/confirmador.js
@@ -0,0 +1,11 @@
+/**
+ * Confirmação de exclusãode um contato
+ * @author Bruno Antunes
+ */
+ 
+ function confirmar(idcon){
+	let resposta = confirm("Confirma a exclusão deste contato?")
+	if(resposta === true){
+		alert()
+	}
+}
